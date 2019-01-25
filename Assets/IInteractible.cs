a851bc91673d88a57495694626b184b87e5a3644
Declan ter Veer-Burke <declantvb@gmail.com>
2019-01-25 22:02:41 +1300

    wip interactibles

---
+++ Assets/IInteractible.cs
@@ -0,0 +1,6 @@
+﻿internal interface IInteractible
+{
+	string[] GetInteractions();
+
+	void Interact(string type);
+}
---
+++ Assets/Interactor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+	private Transform myTransform;
+
+	private string[] currentList;
+	private bool clicked;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		myTransform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		if (Input.GetAxis("Fire1") > 0)
+		{
+			if (!clicked)
+			{
+				var colliders = Physics.OverlapSphere(myTransform.position + myTransform.forward, 1);
+
+				foreach (var collider in colliders)
+				{
+					IInteractible interactible = collider.GetComponentInParent<IInteractible>();
+					if (interactible != null)
+					{
+						//currentList = interactible.GetInteractions();
+						interactible.Interact("test");
+						clicked = true;
+						break;
+					}
+				} 
+			}
+		}
+		else
+		{
+			clicked = false;
+		}
+
+		//if (currentList != null)
+		//{
+		//	Debug.Log("current list : " + string.Join(",", currentList));
+		//}
+	}
+}
